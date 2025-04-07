2737a638cb2c70b462fe1bb50734ff4006183a5b
Monzoor Moshed Mahmud <mmtamal@gmail.com>
2025-04-07 18:04:09 +0600

    add esllint errors

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,40 @@
 import vue from 'eslint-plugin-vue';
+import vueParser from 'vue-eslint-parser';
+import unusedImports from 'eslint-plugin-unused-imports';
+import tseslint from '@typescript-eslint/eslint-plugin';
 
 export default [
   {
+    ignores: ['.nuxt/**/*'],
+  },
+  {
     files: ['**/*.vue'],
     languageOptions: {
-      parser: 'vue-eslint-parser',
+      parser: vueParser,
       parserOptions: {
-        parser: '@typescript-eslint/parser', // If using TypeScript
+        parser: '@typescript-eslint/parser',
         ecmaVersion: 2020,
         sourceType: 'module',
       },
     },
-    plugins: { vue },
+    plugins: {
+      vue,
+      'unused-imports': unusedImports,
+      '@typescript-eslint': tseslint,
+    },
     rules: {
       'vue/multi-word-component-names': 'off',
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
+      '@typescript-eslint/no-unused-vars': ['error'], // <-- requires plugin import
     },
   },
 ];
