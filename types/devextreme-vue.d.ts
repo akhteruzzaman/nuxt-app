ce32d96127dd5bb8d79d0b94d17810d2389b5c25
Monzoor Moshed Mahmud <mmtamal@gmail.com>
2025-04-09 15:45:30 +0600

    pretter rules

--- types/devextreme-vue.d.ts
+++ types/devextreme-vue.d.ts
@@ -8,19 +8,19 @@ import Vue from "vue";
 /* eslint-disable-next-line @typescript-eslint/ban-ts-comment */
 // @ts-ignore
 declare module "devextreme-vue/*" {
-    interface ComponentPublicInstance {
-        $t: (key: string, params?: any) => string;
-    }
+  interface ComponentPublicInstance {
+    $t: (key: string, params?: any) => string;
+  }
 }
 
 /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
 /* eslint-disable-next-line @typescript-eslint/ban-ts-comment */
 // @ts-ignore
 declare module "vue" {
-    interface ComponentCustomProperties {
-        $t: (key: string, params?: any) => string;
-    }
+  interface ComponentCustomProperties {
+    $t: (key: string, params?: any) => string;
+  }
 }
 
 // Add an export to make ESLint treat this as a module
-export { };
+export {};
