ba26d8241940ae5df08c4eab1dd110182157e512
Akhteruzzaman <150516043+akhteruzzaman@users.noreply.github.com>
2025-04-09 10:26:29 +0800

    Pre commit test 2

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,12 @@
 <template>
   <div>
     <h1>Here is your playground !
-      
+
     </h1>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts"
+></script>
 
 <style scoped></style>
