59c2a8c688c80860feaf459c646d6398994a3818
Akhteruzzaman <150516043+akhteruzzaman@users.noreply.github.com>
2025-04-09 10:31:20 +0800

    Pre commit tes5

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,11 @@
 <template>
   <div>
-    <h1>Here is your playground !
-
-    </h1>
+    <h1>Here is your playground !</h1>
   </div>
 </template>
 
-<script setup lang="ts"
-></script>
+<script setup>
+
+</script>
 
 <style scoped></style>
