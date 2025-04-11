9f9e764105bba736e4039964752c55273400f88a
Monzoor Moshed Mahmud <mmtamal@gmail.com>
2025-04-11 12:31:05 +0600

    menu list items

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -18,130 +18,149 @@
         <div class="py-2">
           <div class="px-4 py-2 text-sm text-gray-500">MENU</div>
           <PerfectScrollbar class="ps">
-            <!-- Menu Items -->
-            <div class="relative mx-2 rounded-md">
-              <a
-                href="#"
-                class="flex items-center px-4 py-3 bg-indigo-100 text-indigo-700 rounded-md mr-3 text-sm"
-              >
-                <span class="mr-2 text-indigo-600">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="h-5 w-5"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                  >
-                    <path
-                      d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"
-                    />
-                  </svg>
-                </span>
-                Utama
-              </a>
-              <div
-                class="absolute right-0 top-1/2 transform -translate-y-1/2 w-1 bg-indigo-500 rounded-l-md h-[50px]"
-              />
-            </div>
-
-            <a
-              href="#"
-              class="flex items-center px-6 py-3 text-gray-700 hover:bg-indigo-100 text-sm"
-            >
-              <span class="mr-2">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="h-5 w-5"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
+            <ul class="menu-list">
+              <li class="relative mx-2 rounded-md">
+                <a
+                  href="#"
+                  class="flex items-center px-4 py-3 bg-indigo-100 text-indigo-700 rounded-md mr-3 text-sm"
                 >
-                  <path
-                    d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"
-                  />
-                </svg>
-              </span>
-              Dashboard
-            </a>
+                  <span class="mr-2 text-indigo-600">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="h-5 w-5"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"
+                      />
+                    </svg>
+                  </span>
+                  Utama
+                </a>
+                <div
+                  class="absolute right-0 top-1/2 transform -translate-y-1/2 w-1 bg-indigo-500 rounded-l-md h-[50px]"
+                />
+              </li>
 
-            <a
-              href="#"
-              class="flex items-center justify-between px-6 py-3 text-gray-700 hover:bg-indigo-100 text-sm"
-              @click.prevent="toggleSubMenu('profil')"
-            >
-              <div class="flex items-center">
-                <span class="mr-2">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="h-5 w-5"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                  >
-                    <path
-                      d="M9 2a2 2 0 00-2 2v8a2 2 0 002 2h6a2 2 0 002-2V6.414A2 2 0 0016.414 5L14 2.586A2 2 0 0012.586 2H9z"
-                    />
-                    <path d="M3 8a2 2 0 012-2h2a2 2 0 012 2v8a2 2 0 01-2 2H5a2 2 0 01-2-2V8z" />
-                  </svg>
-                </span>
-                Profil Pembelajaran Dan Peperiksaan
-              </div>
-              <span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="h-4 w-4"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
+              <li>
+                <a
+                  href="#"
+                  class="flex items-center px-6 py-3 text-gray-700 hover:bg-indigo-100 text-sm"
                 >
-                  <path
-                    fill-rule="evenodd"
-                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                    clip-rule="evenodd"
-                  />
-                </svg>
-              </span>
-            </a>
-            <div v-if="openSubMenu === 'profil'" class="ml-6">
-              <a href="#" class="block px-6 py-3 text-gray-700 hover:bg-indigo-100">Test 1</a>
-              <a href="#" class="block px-6 py-3 text-gray-700 hover:bg-indigo-100">Test 2</a>
-            </div>
+                  <span class="mr-2">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="h-5 w-5"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"
+                      />
+                    </svg>
+                  </span>
+                  Dashboard
+                </a>
+              </li>
 
-            <a
-              href="#"
-              class="flex items-center justify-between px-6 py-3 text-gray-700 hover:bg-indigo-100 text-sm"
-              @click.prevent="toggleSubMenu('rekod')"
-            >
-              <div class="flex items-center">
-                <span class="mr-2">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="h-5 w-5"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                  >
-                    <path
-                      d="M2 11a1 1 0 011-1h2a1 1 0 011 1v5a1 1 0 01-1 1H3a1 1 0 01-1-1v-5zM8 7a1 1 0 011-1h2a1 1 0 011 1v9a1 1 0 01-1 1H9a1 1 0 01-1-1V7zM14 4a1 1 0 011-1h2a1 1 0 011 1v12a1 1 0 01-1 1h-2a1 1 0 01-1-1V4z"
-                    />
-                  </svg>
-                </span>
-                Rekod dan Peperiksaan
-              </div>
-              <span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="h-4 w-4"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
+              <li>
+                <a
+                  href="#"
+                  class="flex items-center justify-between px-6 py-3 text-gray-700 hover:bg-indigo-100 text-sm"
+                  @click.prevent="toggleSubMenu('profil')"
                 >
-                  <path
-                    fill-rule="evenodd"
-                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                    clip-rule="evenodd"
-                  />
-                </svg>
-              </span>
-            </a>
-            <div v-if="openSubMenu === 'rekod'" class="ml-6">
-              <a href="#" class="block px-6 py-3 text-gray-700 hover:bg-indigo-100">Rekod 1</a>
-              <a href="#" class="block px-6 py-3 text-gray-700 hover:bg-indigo-100">Rekod 2</a>
-            </div>
+                  <div class="flex items-center">
+                    <span class="mr-2">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        class="h-5 w-5"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                      >
+                        <path
+                          d="M9 2a2 2 0 00-2 2v8a2 2 0 002 2h6a2 2 0 002-2V6.414A2 2 0 0016.414 5L14 2.586A2 2 0 0012.586 2H9z"
+                        />
+                        <path d="M3 8a2 2 0 012-2h2a2 2 0 012 2v8a2 2 0 01-2 2H5a2 2 0 01-2-2V8z" />
+                      </svg>
+                    </span>
+                    Profil Pembelajaran Dan Peperiksaan
+                  </div>
+                  <span>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="h-4 w-4"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                        clip-rule="evenodd"
+                      />
+                    </svg>
+                  </span>
+                </a>
+                <ul v-if="openSubMenu === 'profil'" class="ml-6">
+                  <li>
+                    <a href="#" class="block px-6 py-3 text-gray-700 hover:bg-indigo-100">Test 1</a>
+                  </li>
+                  <li>
+                    <a href="#" class="block px-6 py-3 text-gray-700 hover:bg-indigo-100">Test 2</a>
+                  </li>
+                </ul>
+              </li>
+
+              <li>
+                <a
+                  href="#"
+                  class="flex items-center justify-between px-6 py-3 text-gray-700 hover:bg-indigo-100 text-sm"
+                  @click.prevent="toggleSubMenu('rekod')"
+                >
+                  <div class="flex items-center">
+                    <span class="mr-2">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        class="h-5 w-5"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                      >
+                        <path
+                          d="M2 11a1 1 0 011-1h2a1 1 0 011 1v5a1 1 0 01-1 1H3a1 1 0 01-1-1v-5zM8 7a1 1 0 011-1h2a1 1 0 011 1v9a1 1 0 01-1 1H9a1 1 0 01-1-1V7zM14 4a1 1 0 011-1h2a1 1 0 011 1v12a1 1 0 01-1 1h-2a1 1 0 01-1-1V4z"
+                        />
+                      </svg>
+                    </span>
+                    Rekod dan Peperiksaan
+                  </div>
+                  <span>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="h-4 w-4"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                        clip-rule="evenodd"
+                      />
+                    </svg>
+                  </span>
+                </a>
+                <ul v-if="openSubMenu === 'rekod'" class="ml-6">
+                  <li>
+                    <a href="#" class="block px-6 py-3 text-gray-700 hover:bg-indigo-100"
+                      >Rekod 1</a
+                    >
+                  </li>
+                  <li>
+                    <a href="#" class="block px-6 py-3 text-gray-700 hover:bg-indigo-100"
+                      >Rekod 2</a
+                    >
+                  </li>
+                </ul>
+              </li>
+            </ul>
           </PerfectScrollbar>
         </div>
       </div>
